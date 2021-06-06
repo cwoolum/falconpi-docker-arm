c606db307cf7b15ddd9d24b88f8608b9675a0d7a
Christopher Woolum <christopher.woolum@gmail.com>
2021-06-06 12:34:24 -0700

    try changing the builder

--- build-image.sh
+++ build-image.sh
@@ -1,4 +1,3 @@
-docker buildx create --name fpp-builder
-docker buildx use fpp-builder
+docker buildx create --name fpp-builder --driver docker-container --use
 docker buildx inspect --bootstrap
-docker buildx build --platform linux/arm64,linux/armhf -t cwoolum/fpp:latest --push -f Dockerfile .
+docker buildx build --platform linux/arm64 -t cwoolum/fpp:latest --push -f Dockerfile .
